b35c1b1e5341162a0be56d2bb3c638a2f6b7e64a
snowdot <co.pascu@gmail.com>
2022-10-12 21:24:17 +0300

    add apollo

---
+++ src/apollo/index.ts
@@ -0,0 +1,22 @@
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+    uri: "https://api.stg.cyberconnect.dev/",
+});
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem("accessToken");
+
+    return {
+        headers: {
+            ...headers,
+            Authorization: token ? `bearer ${token}` : "",
+        },
+    };
+});
+
+export const apolloClient = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+});
