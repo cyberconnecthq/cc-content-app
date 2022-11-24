d5facafbde2eb967500e8909bf5f97ea6ab23691
Wei Su <suv@cyberconnect.me>
2022-11-24 12:04:49 +0200

    fix: clear login cache when change account

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,16 @@ import { ApolloProvider } from "@apollo/client";
 import { apolloClient } from "../apollo";
 import { AuthContextProvider } from "../context/auth";
 import { ModalContextProvider } from "../context/modal";
+import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    (window as any).ethereum.on("accountsChanged", function () {
+      // Time to reload your interface with accounts[0]!
+      localStorage.clear();
+      window.location.reload();
+    });
+  }, []);
   return (
     <ApolloProvider client={apolloClient}>
       <AuthContextProvider>
