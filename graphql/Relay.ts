a69608829b66b8fcab1acb4a0961cf73c3962505
Wei Su <suv@cyberconnect.me>
2023-02-02 16:48:08 +0800

    fix: update Relay schema

--- graphql/Relay.ts
+++ graphql/Relay.ts
@@ -1,19 +1,9 @@
 import { gql } from "@apollo/client";
 
 export const RELAY = gql`
-    mutation Relay($input: RelayInput!) {
-        relay(input: $input) {
-            relayTransaction {
-                id
-                txHash
-                typedData {
-                    id
-                    chainID
-                    sender
-                    data
-                    nonce
-                }
-            }
-        }
+  mutation Relay($input: RelayInput!) {
+    relay(input: $input) {
+      relayActionId
     }
+  }
 `;
