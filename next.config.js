05ae15c30521e29175790441b59ac0a2c5ae2783
Waqas Muhammad <m.waqas171288@gmail.com>
2023-02-20 17:23:20 +0500

    setting up environment variables

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_KEY: "", // PINATA API KEY
+    NEXT_PUBLIC_API_SECRET: "", // PINATA API SECRET
+    NEXT_PUBLIC_CYBERCONNECT_API_KEY: "", // CyberConnect API KEY
+    NEXT_PUBLIC_GRAPHQL_ENDPOINT: "https://api.cyberconnect.dev/testnet/",
+    NEXT_PUBLIC_CHAIN_ID: 97
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
